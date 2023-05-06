c44594c1ee6dd24f51903649a747baf480113fbe
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-06 18:55:01 +0300

    feat: exercisesXP 50% done

--- Week2/Day1/ExercisesXP/exercise1.py
+++ Week2/Day1/ExercisesXP/exercise1.py
@@ -1,4 +1,38 @@
+# Print the following output in one line of code:
+#
+# Hello world
+# Hello world
+# Hello world
+# Hello world
 print(('Hello world \n') * 5)
 
+# Write code that calculates the result of: (99^3)*8 (meaning 99 to the power of 3, times 8)
+print((99 ** 3) * 8)
 
-print
+#Create a variable called computer_brand which value is the brand name of your computer.
+#Using the computer_brand variable print a sentence that states the following: "I have a <computer_brand> computer".
+computer_brand = "Mac"
+print('I have a ' + computer_brand + ' computer')
+
+# Create a variable called name, and set it’s value to your name.
+# Create a variable called age, and set it’s value to your age.
+# Create a variable called shoe_size, and set it’s value to your shoe size.
+# Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2 and 3.
+# Have your code print the info message.
+# Run your code
+name = 'Ilona'
+age = '31'
+shoe_size = 38
+info = 'Hi! My name is ' + name + '. I am ' + age + '. I like shopping very much and most of all I like to buy shoes. By the way my shoe size is ' + str(shoe_size) + ' and what about you?'
+print(info)
+
+
+
+
+
+
+
+
+
+
+
