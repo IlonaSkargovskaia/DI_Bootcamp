c023357188e5a1179437322bd49f4a6f08c8f423
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-07 14:03:15 +0300

    feat: dailychallenge done

--- Week2/Day1/DailyChallenge/daily.py
+++ Week2/Day1/DailyChallenge/daily.py
@@ -16,3 +16,17 @@ elif user_str == '':
 first_letter = user_str[0]
 last_letter = user_str[len(user_str) - 1]
 print(f'First letter from your string is {first_letter} and the last letter is {last_letter}')
+
+# 3. Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. 
+
+for x in range(len(user_str)):
+    print(user_str[: x + 1])
+
+
+#4. Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method). 
+
+import random
+
+new_str = list(user_str)
+random.shuffle(new_str)
+print(''.join(new_str))
