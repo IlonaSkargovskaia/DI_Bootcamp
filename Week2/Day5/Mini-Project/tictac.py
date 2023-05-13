a9b46e6124465ac52a2bfee93987b4c34c59fa0d
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-13 11:43:57 +0300

    feat: done if one from players wins

--- Week2/Day5/Mini-Project/tictac.py
+++ Week2/Day5/Mini-Project/tictac.py
@@ -1,30 +1,19 @@
 #create_board
 
-#шаги:
-# 1. нарисовать поле
-# 2. создать смену игроков Х и О
-# 3. Начинает игру Х, задаем вопрос: "Какую строку?" "какой столбец?"
-# 4. например [1][0] - присваиваем сюда "Х"
-# 5. начинает ход О..
-# 6. Создать все варианты победы и затем проверять да или нет
-# 7.
-
-import numpy as np
-
 def init_board():
     #массив с пустыми ячейками
-    board = np.empty(shape=(3,3), dtype='object')
-    for row in range(3):
-        for col in range(3):
-            board[row][col] = " "
-    print(board)
+    board = [[' ', ' ', ' '],
+             [' ', ' ', ' '],
+             [' ', ' ', ' ']]
     return board
 
 board = init_board()
 
+# print board for players
 def draw_board():
     
     print(f"TIC TAC TOE")
+    print(f"Player 1: 'X'   Player 2: 'O' ")
     print(f"*********************")
     print(f"*   {board[0][0]}  |  {board[0][1]}  |  {board[0][2]}   *")
     print(f"*  --- | --- | ---  *")
@@ -34,3 +23,73 @@ def draw_board():
     print(f"*********************")
 
 draw_board()
+
+
+#change board after than player turn
+def update_board(row, col, player):
+    board[row][col] = player
+
+#проверяю пустое ли место
+def is_place_empty(row, col):
+    return board[row][col] == " "
+
+
+#проверка победителя
+def check_winner(player):
+    #horizontal
+    for row in range(3):
+        if(board[row][0] == player and board[row][1] == player and board[row][2] == player):
+            return True
+    #vertical
+    for col in range(3):
+        if(board[0][col] == player and board[1][col] == player and board[2][col] == player):
+            return True
+    #left-diagonal
+    if(board[0][0] == player and board[1][1] == player and board[2][2] == player):
+        return True
+    #right-diagonal
+    if(board[0][2] == player and board[1][1] == player and board[2][0] == player):
+        return True
+    
+#structure game
+turn = 0
+game_over = False
+player = "X"
+
+#пока не конец игры
+while not game_over:
+    #спрашиваюю игроков по очереди
+    if turn == 0:
+        row = int(input("Player 1: enter row (1 - 3): ")) - 1
+        col = int(input("Player 1: enter column (1 - 3): ")) - 1
+    if turn == 1:
+        row = int(input("Player 2: enter row (1-3): ")) - 1
+        col = int(input("Player 2: enter column (1-3): ")) - 1
+
+        while not is_place_empty(row, col):
+            print("This place is busy. Choose another")
+            if turn == 0:
+                row = int(input("Player 1: enter row (1 - 3): ")) - 1
+                col = int(input("Player 1: enter column (1 - 3): ")) - 1
+            if turn == 1:
+                row = int(input("Player 2: enter row (1-3): ")) - 1
+                col = int(input("Player 2: enter column (1-3): ")) - 1
+    
+    update_board(row, col, player)
+    draw_board()
+
+#game over if conditions from check_winner is true
+    if check_winner(player):
+        game_over = True
+        print(f"Player '{player}' is winner! Congratulations")
+
+#which player turn
+    turn += 1
+    turn = turn % 2
+
+#change players
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
+
