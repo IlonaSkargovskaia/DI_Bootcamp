ca3aa59747ab8d88b2ce2a6233442147502736a2
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-13 19:59:04 +0300

    feat: ex_1 and ex_2 from Day2 Gold done

---
+++ Week2/Day2/ExerciseXPGold/gold.py
@@ -0,0 +1,19 @@
+# Write code that concatenates two lists together without using the + sign.
+
+def ex_1():
+    list1 = [1,2,5,8]
+    list2 = [3,6,8,9]
+
+    for i in list2:
+        list1.append(i)
+    print(list1)
+ex_1()
+
+#Create a loop that goes from 1500 to 2500 and prints all multiples of 5 and 7.
+
+def ex_2():
+    for i in range(1500, 2500):
+        if i % 5 == 0 and i % 7 == 0:
+            print(i)
+
+ex_2()
