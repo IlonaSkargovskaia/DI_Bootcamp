fd58d889e16ff2f16effc9661884d0da93712921
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-13 23:03:57 +0300

    feat: day2 GoldXP ex6

--- Week2/Day2/ExerciseXPGold/gold.py
+++ Week2/Day2/ExerciseXPGold/gold.py
@@ -1,44 +1,55 @@
 # Write code that concatenates two lists together without using the + sign.
 
-def ex_1():
-    list1 = [1,2,5,8]
-    list2 = [3,6,8,9]
+# def ex_1():
+#     list1 = [1,2,5,8]
+#     list2 = [3,6,8,9]
 
-    for i in list2:
-        list1.append(i)
-    print(list1)
-ex_1()
+#     for i in list2:
+#         list1.append(i)
+#     print(list1)
+# ex_1()
 
-#Create a loop that goes from 1500 to 2500 and prints all multiples of 5 and 7.
+# #Create a loop that goes from 1500 to 2500 and prints all multiples of 5 and 7.
 
-def ex_2():
-    for i in range(1500, 2500):
-        if i % 5 == 0 and i % 7 == 0:
-            print(i)
+# def ex_2():
+#     for i in range(1500, 2500):
+#         if i % 5 == 0 and i % 7 == 0:
+#             print(i)
 
 
 
 #ex3
 
-names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+# names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
-user_answer = input("Write your name: ")
-if user_answer in names:
-    print(names.index(user_answer))
+# user_answer = input("Write your name: ")
+# if user_answer in names:
+#     print(names.index(user_answer))
 
 
 #ex4
 
-num1 = int(input("Write 1 number: "))
-num2 = int(input("Write 2 number: "))
-num3 = int(input("Write 3 number: "))
+# num1 = int(input("Write 1 number: "))
+# num2 = int(input("Write 2 number: "))
+# num3 = int(input("Write 3 number: "))
 
-max_num = max(num1, num2, num3)
-print(max_num)
+# max_num = max(num1, num2, num3)
+# print(max_num)
 
 
 #ex5
-alpha = "abcdefghijklmnopqrstuvwxyz"
-for letter in alpha:
-    print(letter)
+# alpha = "abcdefghijklmnopqrstuvwxyz"
+# for letter in alpha:
+#     print(letter)
+
+
+#ex6
+users_words = list(input("Write 7 words separate by comma: ").split(','))
+let = input("Write 1 letter: ")
+
+for item in users_words:
+    if let in item:
+        print(users_words.index(item))
+    else:
+        print("This letter is not in any word ")
 
