85827fed2f874d20accdfe2979dd1c94205a2756
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-13 10:50:20 +0300

    feat: draw board with numpy

--- Week2/Day5/Mini-Project/tictac.py
+++ Week2/Day5/Mini-Project/tictac.py
@@ -7,72 +7,30 @@
 # 4. например [1][0] - присваиваем сюда "Х"
 # 5. начинает ход О..
 # 6. Создать все варианты победы и затем проверять да или нет
-# 7. 
+# 7.
 
-place = [[" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]]
+import numpy as np
 
-line0 = f"*********************"
-line1 = f"*   {place[0][0]}  |  {place[0][1]}  |  {place[0][2]}   *"
-line2 = f"*  --- | --- | ---  *"
-line3 = f"*   {place[1][0]}  |  {place[1][1]}  |  {place[1][2]}   *"
-line4 = f"*  --- | --- | ---  *"
-line5 = f"*   {place[2][0]}  |  {place[2][1]}  |  {place[2][2]}   *"
-line6 = f"*********************"
+def init_board():
+    #массив с пустыми ячейками
+    board = np.empty(shape=(3,3), dtype='object')
+    for row in range(3):
+        for col in range(3):
+            board[row][col] = " "
+    print(board)
+    return board
 
+board = init_board()
 
-#start printing board
-def display_board():
-    print("Player 1: X  and  Player 2: O")
-    print("TIC TAC TOE")
-    print(line0)
-    print(line1)
-    print(line2)
-    print(line3)
-    print(line4)
-    print(line5)
-    print(line6)
-display_board()
+def draw_board():
+    
+    print(f"TIC TAC TOE")
+    print(f"*********************")
+    print(f"*   {board[0][0]}  |  {board[0][1]}  |  {board[0][2]}   *")
+    print(f"*  --- | --- | ---  *")
+    print(f"*   {board[1][0]}  |  {board[1][1]}  |  {board[1][2]}   *")
+    print(f"*  --- | --- | ---  *")
+    print(f"*   {board[2][0]}  |  {board[2][1]}  |  {board[2][2]}   *")
+    print(f"*********************")
 
-#change players
-def change_players():
-    turn = 0
-    pl = "X"
-
-    while True:
-        #задаю вопросы по очереди
-        #Player 1 turns
-        if turn == 0:
-            print("\nplayer 1 turn ...\n")
-            row = int(input("Enter a row (1 - 3): ")) - 1
-            col = int(input("Enter a column (1 - 3): ")) - 1
-            
-        #Player 2 turns
-        if turn == 1:
-            print("\nplayer 2 turn ...\n")
-            row = int(input("Enter a row (1 - 3): ")) - 1
-            col = int(input("Enter a column (1 - 3): ")) - 1
-            
-        #чередую ходы
-        turn += 1
-        print(turn)
-        turn = turn % 2
-        print(turn)
-
-        #чередую игроков
-        if pl == "X":
-            pl = "O"
-        else:
-            pl = "X"
-          
-change_players()
-
-
-#checking board if empty 
-
-def is_place_empty(row, col):
-    return place[row][col] == " "
-is_place_empty()
-
-
+draw_board()
