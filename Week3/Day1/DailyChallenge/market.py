fb5c7a38ea1cb9910ea3644ef6ef41fc66ac721f
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-14 20:26:00 +0300

    feat: formatting strings Daily W3D1

--- Week3/Day1/DailyChallenge/market.py
+++ Week3/Day1/DailyChallenge/market.py
@@ -5,20 +5,28 @@ class Farm:
         self.animals = {}
 
     def add_animal(self, new_animal, num_animals = 1):
+        #adding to dictionary
         if new_animal not in self.animals.keys():
             self.animals[new_animal] = num_animals
         else:
             self.animals[new_animal] += num_animals
         
+    def get_info(self):
+        print(f"{self.name}\n")
 
+        for kind, count in self.animals.items():
+            print(f"{kind} : {count}")
+
+        print(f"\n\tE-I-E-I-0!")
+        
 
 macdonald = Farm("McDonald's farm")
 macdonald.add_animal('cow', 5)
 macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
 
-print(f"{macdonald.animals}")
 
 
 
