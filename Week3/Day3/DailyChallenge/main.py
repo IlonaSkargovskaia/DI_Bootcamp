f0ea677a7d446d78ef225f2b2129f95fea64afc7
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-19 17:53:09 +0300

    fix: W3D3 circle DCh adding functions

--- Week3/Day3/DailyChallenge/main.py
+++ Week3/Day3/DailyChallenge/main.py
@@ -13,31 +13,53 @@
 
 
 class Circle:
-    def __init__(self, radius = 0, diameter = 0):
+    circles_list = []
+
+    def __init__(self, name, radius = 0, diameter = 0):
         self.radius = float(radius)
         self.diameter = float(diameter)
+        self.name = name
+        Circle.circles_list.append(self)
+
+    def __str__(self):
+        sentence = f'{self.name} with radius {self.radius} and diameter {self.diameter}'
+        return sentence
 
 #Calculate the diameter getting the radius as arg
     @classmethod
-    def from_radius(cls, radius):
-        return cls(radius = radius, diameter = radius*2)
+    def from_radius(cls, name, radius):
+        return cls(name = name, radius = radius, diameter = radius*2)
 
 #Calculate the radius getting the diameter as arg
     @classmethod
-    def from_diameter(cls, diameter):
-        
-        return cls(diameter = diameter, radius = diameter/2)
-#area   
+    def from_diameter(cls, name, diameter):
+        return cls(name = name, diameter = diameter, radius = diameter/2)
+#calculate the area   
     def get_area(self) -> float:
         return 3.14 * self.radius ** 2
     
-#two areas   
+#add two circle areas   
     def __add__(self, other_circle) -> float:
         return self.get_area() + other_circle.get_area()
+    
+#function with other
+    def __gt__(self, other_circle) :
+        if self.get_area() > other_circle.get_area() :
+            print(f'{self.name} is bigger than {other_circle.name}')
+        elif other_circle.get_area() > self.get_area() :
+            print(f'{other_circle.name} is bigger than {self.name}')
 
-circleA = Circle.from_diameter(diameter = 4)
-circleB = Circle.from_radius(radius = 4)
-circleC = Circle(3,6)
+
+    def sort_circles(self) :
+       sorted_circles = sorted(Circle.circles_list, key=lambda x: x.get_area())
+       print(sorted_circles)
+       return sorted_circles
+    
+    
+
+circleA = Circle.from_diameter('circleA', diameter = 4)
+circleB = Circle.from_radius('circleB', radius = 4)
+circleC = Circle('circleC', 3,6)
 circleA.get_area()
 
 print("CircleA radius : ", circleA.radius, "CircleA diameter: ", circleA.diameter)
@@ -45,7 +67,15 @@ print("CircleB radius : ", circleB.radius, "CircleB diameter: ", circleB.diamete
 print("CircleC radius : ", circleC.radius, "CircleC diameter: ", circleC.diameter)
 print("circleA.get_area : ", circleA.get_area())
 print("circleB.get_area : ", circleB.get_area())
+
 # adding Circles and rouding the float
 print('add A and B: ', round(circleA + circleB, 2)) 
 
+#greater than...
+(circleC > circleB)
+(circleA > circleB)
+
+#sorting circles
+for name in circleA.sort_circles():
+    print(name)
 
--- Week3/Day5/DailyChallenge/cards.py
+++ Week3/Day5/DailyChallenge/cards.py
@@ -9,25 +9,24 @@
 # What is polymorphism?
 # What is method resolution order or MRO?
 
-print("Class - is like an object constructor, or a 'blueprint' for creating objects.")
+# print("1. Class - is like an object constructor, or a 'blueprint' for creating objects.")
 
-print("Instance is an individual object of a certain class")
+# print("2. Instance is an individual object of a certain class")
 
-print("Encapsulation refers to the bundling of attributes and methods inside a single class")
+# print("3. Encapsulation refers to the bundling of attributes and methods inside a single class")
 
-print("Abstraction is a process of handling complexity by hiding unnecessary information from the user")
+# print("4. Abstraction is a process of handling complexity by hiding unnecessary information from the user")
 
-print("Inherintance allows us to define a class that inherits all the methods and properties from another class (Parent and child classes)")
+# print("5. Inherintance allows us to define a class that inherits all the methods and properties from another class (Parent and child classes)")
 
-print("When a class is derived from more than one base class it is called multiple Inheritance. The derived class inherits all the features of the base case")
+# print("6. When a class is derived from more than one base class it is called multiple Inheritance. The derived class inherits all the features of the base case")
 
-print("polymorphism means having many forms. For example we have one name of function, which we use in different classes, but this function performs different things")
+# print("7. polymorphism means having many forms. For example we have one name of function, which we use in different classes, but this function performs different things")
 
-print("It is the order in which a method is searched for in a classes hierarchy")
+# print("8. It is the order in which a method is searched for in a classes hierarchy")
 
 
 #The Deck of cards class should NOT inherit from a Card class.
-
 # The requirements are as follows:
 
 # The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
@@ -38,24 +37,30 @@ print("It is the order in which a method is searched for in a classes hierarchy"
 import random
 
 class Card:
-
+    # suit (Hearts, Diamonds, Clubs, Spades) 
+    # value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
     def __init__(self, suit, value):
         self.suit = suit
         self.value = value
 
+card_suits = ['hearts', 'diamonds', 'clubs', 'spades']
+card_values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
 class Deck:
-
     def __init__(self):
-        self.card_deck = []
+        self.cards_deck = []
 
-    def suffle(self):
-        if len(self.card_deck) == 52:
-            random.shuffle(self.card_deck)
+    def shuffle(self):
+        if len(self.cards_deck) == 52:
+            random.shuffle(self.cards_deck)
+        print(self.cards_deck)
 
-        print(self.card_deck)   
+#obj in class Deck
+deck = Deck()
 
-card_suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-card_value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+for suit in card_suits:
+    for value in card_values:
+        a = Card(suit, value)
+        deck.cards_deck.append(a)
 
-
-
+deck.shuffle()
