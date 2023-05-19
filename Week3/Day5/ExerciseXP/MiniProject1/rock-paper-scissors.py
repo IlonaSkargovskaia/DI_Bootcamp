6fd841f210d6f512056b21d7a72b290cd85efca1
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-19 22:00:18 +0300

    init: start working with XP from Day5 week3

---
+++ Week3/Day5/ExerciseXP/MiniProject1/rock-paper-scissors.py
@@ -0,0 +1,20 @@
+#What You Will Create
+# Mini-Project: Rock, Paper, Scissors
+# Rock-paper-scissors is an old game that can be played between two people. You can read about it in wikipedia
+
+# We will create a game for the user to play Rock-paper-scissors against the computer.
+
+# The user will input his/her move (rock/paper/scissors),
+# and the computer will select either rock, paper or scissors at random.
+# We will then compare the user’s move with the computer’s move, and determine the results of the game:
+
+# The user won
+
+# The computer won (the user lost)
+# A draw (tie)
+# We will print the outcome of each game: the user’s choice, the computer’s choice, and the result.
+
+# The user will be able to play again and again. Once the user decides to exit the program, we will print a summary of the outcomes of all the games: how many times they won, lost or and tied the computer.
+
+
+
---
+++ Week3/Day5/ExerciseXP/MiniProject2/anagram_checker.py
@@ -0,0 +1,4 @@
+#Anagram Checker
+# We will create a program that will ask the user for a word.
+# It will check if the word is a valid English word, and then find all possible anagrams for that word.
+
