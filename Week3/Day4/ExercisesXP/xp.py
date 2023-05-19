81f76e74e0713c7a689eef7507b1b24ef046bf46
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-19 22:29:46 +0300

    feat: anagram 1task

--- Week3/Day4/ExercisesXP/xp.py
+++ Week3/Day4/ExercisesXP/xp.py
@@ -1,74 +1,74 @@
-# # 🌟 Exercise 1 – Random Sentence Generator
-# # Instructions
-# # Description: In this exercise we will create a random sentence generator. We will do this by asking the user how long the sentence should be and then printing the generated sentence.
+# 🌟 Exercise 1 – Random Sentence Generator
+# Instructions
+# Description: In this exercise we will create a random sentence generator. We will do this by asking the user how long the sentence should be and then printing the generated sentence.
 
-# # Hint : The generated sentences do not have to make sense.
+# Hint : The generated sentences do not have to make sense.
 
-# # Download this word list
+# Download this word list
 
-# # Save it in your development directory.
+# Save it in your development directory.
 
-# # Create a function called get_words_from_file. This function should read the file’s content and return the words as a collection. What is the correct data type to store the words?
+# Create a function called get_words_from_file. This function should read the file’s content and return the words as a collection. What is the correct data type to store the words?
 
-# # Create another function called get_random_sentence which takes a single parameter called length. The length parameter will be used to determine how many words the sentence should have. The function should:
-# # use the words list to get your random words.
-# # the amount of words should be the value of the length parameter.
+# Create another function called get_random_sentence which takes a single parameter called length. The length parameter will be used to determine how many words the sentence should have. The function should:
+# use the words list to get your random words.
+# the amount of words should be the value of the length parameter.
 
-# # Take the random words and create a sentence (using a python method), the sentence should be lower case.
+# Take the random words and create a sentence (using a python method), the sentence should be lower case.
 
-# # Create a function called main which will:
+# Create a function called main which will:
 
-# # Print a message explaining what the program does.
+# Print a message explaining what the program does.
 
-# # Ask the user how long they want the sentence to be. Acceptable values are: an integer between 2 and 20. Validate your data and test your validation!
-# # If the user inputs incorrect data, print an error message and end the program.
-# # If the user inputs correct data, run your code.
-# import random
+# Ask the user how long they want the sentence to be. Acceptable values are: an integer between 2 and 20. Validate your data and test your validation!
+# If the user inputs incorrect data, print an error message and end the program.
+# If the user inputs correct data, run your code.
+import random
 
-# def get_words_from_file(file):
+def get_words_from_file(file):
 
-#     with open(file, 'r') as file:
-#         # print(words.read()) #every word in one line
-#         words_collection = []
-#         for word in file.readlines():
-#             words_collection.append(word.strip())
-#         #print(words_collection)
-#         return words_collection
-
-
-# get_words_from_file("Week3/Day4/ExercisesXP/words.txt")
-
-# def get_random_sentence(length):
-#     sentence = []
-#     words_for_sentence = get_words_from_file("Week3/Day4/ExercisesXP/words.txt")
-#     for word in range(length):
-#         sentence.append(random.choice(words_for_sentence).lower())
-#     print(' '.join(sentence))
-
-# get_random_sentence(5)
+    with open(file, 'r') as file:
+        # print(words.read()) #every word in one line
+        words_collection = []
+        for word in file.readlines():
+            words_collection.append(word.strip())
+        #print(words_collection)
+        return words_collection
 
 
-# def main():
-#     print("My program asking user to input sentence length and give him sentence with random words of that length")
+get_words_from_file("Week3/Day4/ExercisesXP/words.txt")
+
+def get_random_sentence(length):
+    sentence = []
+    words_for_sentence = get_words_from_file("Week3/Day4/ExercisesXP/words.txt")
+    for word in range(length):
+        sentence.append(random.choice(words_for_sentence).lower())
+    print(' '.join(sentence))
+
+get_random_sentence(5)
+
+
+def main():
+    print("My program asking user to input sentence length and give him sentence with random words of that length")
     
-#     user_length = input("How long do you want to be the sentence? Write the number (2 - 20): ")
+    user_length = input("How long do you want to be the sentence? Write the number (2 - 20): ")
 
-#     try:
-#         user_length = int(user_length)
-#         # if user_length != int:
-#         #     raise TypeError("you give me a wrong datatype")
-#     except TypeError as error:
-#         print("TYPE ERROR")
-#         print(error)
+    try:
+        user_length = int(user_length)
+        # if user_length != int:
+        #     raise TypeError("you give me a wrong datatype")
+    except TypeError as error:
+        print("TYPE ERROR")
+        print(error)
     
-#     except ValueError as error:
-#         print("Write please numbers between 2- 20")
-#         print(error)
+    except ValueError as error:
+        print("Write please numbers between 2- 20")
+        print(error)
         
-#     else:
-#         get_random_sentence(user_length)
+    else:
+        get_random_sentence(user_length)
 
-# main()
+main()
 
 
 #🌟 Exercise 2: Working With JSON
--- Week3/Day5/ExerciseXP/MiniProject2/anagram_checker.py
+++ Week3/Day5/ExerciseXP/MiniProject2/anagram_checker.py
@@ -2,3 +2,23 @@
 # We will create a program that will ask the user for a word.
 # It will check if the word is a valid English word, and then find all possible anagrams for that word.
 
+class AnagramChecker:
+
+    def __init__(self):
+        with open("Week3/Day5/ExerciseXP/MiniProject2/sowpods.txt", "r") as file:
+                    print(file.read())
+
+    def is_valid_word(self):
+        user_word = input("Write any word: ").upper()
+        eng_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        new_user_word = ''
+        for letter in user_word:
+            if letter in eng_alphabet:
+                new_user_word += letter
+            else:
+                print("Try again, your word is invalid")
+        print(new_user_word)
+
+
+word = AnagramChecker()
+word.is_valid_word()
