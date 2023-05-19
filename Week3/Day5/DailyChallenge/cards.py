334deb029c71295a308341152f9cc188365d8bb1
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-19 21:40:11 +0300

    fix: W3D5 DailyCh remove some spaces

--- Week3/Day5/DailyChallenge/cards.py
+++ Week3/Day5/DailyChallenge/cards.py
@@ -9,21 +9,14 @@
 # What is polymorphism?
 # What is method resolution order or MRO?
 
-# print("1. Class - is like an object constructor, or a 'blueprint' for creating objects.")
-
-# print("2. Instance is an individual object of a certain class")
-
-# print("3. Encapsulation refers to the bundling of attributes and methods inside a single class")
-
-# print("4. Abstraction is a process of handling complexity by hiding unnecessary information from the user")
-
-# print("5. Inherintance allows us to define a class that inherits all the methods and properties from another class (Parent and child classes)")
-
-# print("6. When a class is derived from more than one base class it is called multiple Inheritance. The derived class inherits all the features of the base case")
-
-# print("7. polymorphism means having many forms. For example we have one name of function, which we use in different classes, but this function performs different things")
-
-# print("8. It is the order in which a method is searched for in a classes hierarchy")
+print("1. Class - is like an object constructor, or a 'blueprint' for creating objects.")
+print("2. Instance is an individual object of a certain class")
+print("3. Encapsulation refers to the bundling of attributes and methods inside a single class")
+print("4. Abstraction is a process of handling complexity by hiding unnecessary information from the user")
+print("5. Inherintance allows us to define a class that inherits all the methods and properties from another class (Parent and child classes)")
+print("6. When a class is derived from more than one base class it is called multiple Inheritance. The derived class inherits all the features of the base case")
+print("7. polymorphism means having many forms. For example we have one name of function, which we use in different classes, but this function performs different things")
+print("8. It is the order in which a method is searched for in a classes hierarchy")
 
 
 #The Deck of cards class should NOT inherit from a Card class.
