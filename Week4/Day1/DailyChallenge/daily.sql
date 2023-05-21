292a7052b931a67ac081c88ad5ef5e943b783a46
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-21 18:03:56 +0300

    feat: dailyChallenge done W4D1

---
+++ Week4/Day1/DailyChallenge/daily.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM actor
+
+-- sum of actors
+SELECT COUNT(*) FROM actor
+
+-- INSERT INTO actor (first_name, last_name, birthdate, number_oscars)
+-- VALUES
+-- (null, null, null, null)
+-- null value in column "first_name" of relation "actor" violates not-null constraint
