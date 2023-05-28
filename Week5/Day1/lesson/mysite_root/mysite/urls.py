d33e844028374cd2063baa249071163a198a55eb
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-29 00:37:24 +0300

    fex: bugs with links in two exercises

--- Week5/Day1/lesson/mysite_root/mysite/urls.py
+++ Week5/Day1/lesson/mysite_root/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for animals project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('info.urls')),
+    path('', include('polls.urls')), # include the urls.py from the polls application
+    
 ]
