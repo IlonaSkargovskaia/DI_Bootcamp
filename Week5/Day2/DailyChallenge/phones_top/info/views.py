630f630e4ffb47123f0fc6698f32f884fd8be92f
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-30 08:46:30 +0300

    fix: bugs with condition if

--- Week5/Day2/DailyChallenge/phones_top/info/views.py
+++ Week5/Day2/DailyChallenge/phones_top/info/views.py
@@ -2,16 +2,27 @@ from django.shortcuts import render
 from .models import Person
 
 def phonenumber(request, number):
+
+    try:
+        person = Person.objects.get(phone_number = number)
+    except Person.DoesNotExist:
+        person = None
+
     context = {
-        'person' : Person.objects.get(phone_number = number)
+        'person' : person
     }
 
     return render(request, 'number.html', context)
 
 def name(request, input_name):
+    #нужно перехватить ошибку чтобы перешло к else
+    try: 
+        person = Person.objects.get(name = input_name)
+    except Person.DoesNotExist:
+        person = None
+
     context = {
-        #получаю имя и сравниваю с введенным пользователем
-        'person' : Person.objects.get(name = input_name)
+        'person' : person
     }
 
     return render(request, 'name.html', context)
