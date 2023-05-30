a8b53bfd18f26bad1e7a5ce273e11589e41e0064
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-05-31 00:43:50 +0300

    feat: W5D3 done (without forms)

--- Week5/Day3/ExerciseXP/gifs_app_top/gifs/urls.py
+++ Week5/Day3/ExerciseXP/gifs_app_top/gifs/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 # one urlpattern per line
 urlpatterns = [
-    path('', views.homepage, name='home')
+    path('', views.homepage, name='home'),
+    path('category/<int:cat_id>/', views.category, name='category'),
+    path('gif_view/<int:gif_id>/', views.gif_view, name='gif_view'),
+    path('categories/', views.categories, name='categories'),
 ]
