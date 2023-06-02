45c1f18b953ed4d9f4e6ec3a1ba59e2054c54bd3
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-02 19:23:59 +0300

    feat: start daily W5D5

---
+++ Week5/Day5/DailyChallenge/phones_top/info/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# (вызываем созданную там функцию)
+urlpatterns = [
+    path('persons/phonenumber/<str:number>/', views.phonenumber, name="phone"),
+    path('persons/name/<str:input_name>/', views.name, name='name'),
+    path('search-phone/', views.search_phone, name='search_phone'),
+]
---
+++ Week5/Day5/DailyChallenge/phones_top/info/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from .models import Person
+from .forms import PhoneForm
+
+def search_phone(request):
+    
+    if request.method == 'POST':
+        query = request.POST['search']
+
+        if PhoneForm({'number' : query}).is_valid():
+            return redirect('phone', number = query)
+        else:
+            return redirect('name', input_name = query)
+
+    return render(request, 'search-phone.html')
+    
+
+def phonenumber(request, number):
+
+    try:
+        person = Person.objects.get(phone_number = number)
+    except Person.DoesNotExist:
+        person = None
+
+    context = {
+        'person' : person
+    }
+
+    return render(request, 'number.html', context)
+
+
+def name(request, input_name):
+    #нужно перехватить ошибку чтобы перешло к else
+    try: 
+        person = Person.objects.get(name = input_name)
+    except Person.DoesNotExist:
+        person = None
+
+    context = {
+        'person' : person
+    }
+
+    return render(request, 'name.html', context)
