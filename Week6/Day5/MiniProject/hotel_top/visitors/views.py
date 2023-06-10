a41248f923463cfd73fd3a3723b74df7335b1abf
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-10 10:25:19 +0300

    feat: first part MiniProject done

---
+++ Week6/Day5/MiniProject/hotel_top/visitors/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import ListView, DetailView, View
+from django.urls import reverse_lazy 
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+
+
+def homepage(request):
+
+    context = {
+        'title' : 'Homepage HOTEL Booking'
+    }
+
+    return render(request, 'home.html', context)
