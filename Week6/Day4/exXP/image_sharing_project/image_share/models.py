84d4b574cc79ba9ea6608206c98d5b0ce48db38b
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-17 00:07:58 +0300

    feat: W6 Day4 XP done

--- Week6/Day4/exXP/image_sharing_project/image_share/models.py
+++ Week6/Day4/exXP/image_sharing_project/image_share/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
 
 class Image(models.Model):
 
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images')
     description = models.TextField()
-    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -19,3 +20,5 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
--- Week6/Day4/exXP/image_sharing_project/image_share/urls.py
+++ Week6/Day4/exXP/image_sharing_project/image_share/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    
-]
+    path('', views.HomePageView.as_view(), name="home"),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('upload-image/', views.ImageCreateView.as_view(), name='upload_image'),
+    path('my-images/', views.MyImagesView.as_view(), name='my_images'),
+    path('album/<pk>', views.UsersImagesView.as_view(), name='album'),
+    path('profile/<pk>', views.UserProfileView.as_view(), name='profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
