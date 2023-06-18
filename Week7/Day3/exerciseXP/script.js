60364bcc56dacc205951e28f2988f000bda994e1
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-18 11:34:10 +0300

    feat: exXP W7D3 3 parts done

--- Week7/Day3/exerciseXP/script.js
+++ Week7/Day3/exerciseXP/script.js
@@ -72,3 +72,20 @@ for (let i = 0; i < colors.length; i++) {
     }
     
 }
+
+
+// Prompt the user for a number.
+// Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+let answer = prompt("Write the number: ")
+console.log(typeof answer)
+
+// While the number is smaller than 10 continue asking the user for a new number.
+// Tip : Which while loop is more relevant for this situation?
+
+
+do {
+    answerLoop = prompt("Write the number: ")
+    console.log(answerLoop)
+}
+while (parseInt(answerLoop) < 10);
