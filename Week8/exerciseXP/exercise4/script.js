01b09aef455a96dd7ce8c2f938261f32009bfd9d
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-25 22:02:12 +0300

    feat: ex4 from XP W8D1 done

--- Week8/exerciseXP/exercise4/script.js
+++ Week8/exerciseXP/exercise4/script.js
@@ -1,6 +1,15 @@
 // Exercice 4 : Volume Of A Sphere
 
 function volumeSphere(){
-    const form = document.forms
-    const radius = 
+    const form = document.forms['MyForm'];
+    const input = form.elements.radius;
+    let volume = form.elements.volume;
+    const button = form.elements.submit;
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        volume.value = (4/3 * 3.14 * (input.value) ** 3).toFixed(2); 
+    })
 }
+
+volumeSphere()
