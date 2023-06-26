8f8c9b5b7024e54b1da695d386e5022f4af6ed9b
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-26 20:46:31 +0300

    feat: daily W8D2 done

---
+++ Week8/Day2/DailyChallenge/script.js
@@ -0,0 +1,48 @@
+// Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
+
+// Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method.
+
+// Create another arrow function named cloneGroceries.
+// In the function, create a variable named user that is a copy of the client variable. (Tip : make the user variable equal to the client variable)
+// Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
+// In the function, create a variable named shopping that is equal to the groceries variable.
+// Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
+// Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ?
+
+// Invoke the cloneGroceries function.
+
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+const displayGroceries = () => groceries.fruits.forEach(el => console.log(el));
+displayGroceries();
+
+const cloneGroceries = () => {
+    let user = client;
+    client = 'Betty';
+
+    let shopping = groceries;
+    groceries.totalPrice = '35$';
+    groceries.other.payed = false;
+
+
+    console.log(client);
+    console.log(user);
+    console.log(shopping);
+    console.log(groceries);
+}
+
+cloneGroceries();
+
+
+// Will we also see this modification in the user variable ? Why ?
+//  Because in simple types we pass by value
