79ad5d6d7d41b23c6f3ec53c98e41efbdf97edca
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-28 23:10:19 +0300

    feat: first part MiniProject almost done

---
+++ Week8/Day5/MiniProject/js/script.js
@@ -0,0 +1,122 @@
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'img/robots/1.png'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'img/robots/2.png'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'img/robots/3.png'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'img/robots/4.png'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'img/robots/5.png'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'img/robots/6.png'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'img/robots/7.png'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'img/robots/8.png'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'img/robots/9.png'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+];
+
+
+class Robots {
+    constructor ({name, username, email, image}) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.image = image;
+    }
+
+    displayRobots() {
+        const row = document.querySelector('.row__content');
+//card__outer
+        const robotCard = document.createElement('div');
+        robotCard.classList.add('card__item');
+//card__inner
+        const cardContent = document.createElement('div');
+        cardContent.classList.add('card__content');
+//card__image
+        const robotImage = document.createElement('img');
+        robotImage.classList.add('card__img');
+        robotImage.src = this.image;
+//card__title
+        const robotTitle = document.createElement('h3');
+        robotTitle.classList.add('card__title');
+        const title = document.createTextNode(this.name);
+//email
+        const robotEmail = document.createElement('p');
+        robotEmail.classList.add('card__email');
+        const emailText = document.createTextNode(this.email);
+
+//img
+        cardContent.appendChild(robotImage);
+//title
+        robotTitle.appendChild(title);
+        cardContent.appendChild(robotTitle);
+//email
+        robotEmail.appendChild(emailText);
+        cardContent.appendChild(robotEmail);
+
+//display card
+        robotCard.appendChild(cardContent);
+        row.appendChild(robotCard);
+    }
+}
+
+robots.forEach(element => {
+    const robot = new Robots(element);
+    robot.displayRobots();
+})
