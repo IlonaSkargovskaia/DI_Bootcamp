7155686111343dc15849ee71a1d0aa83071704bb
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-06-29 09:52:12 +0300

    fix: MiniProject

--- Week8/Day5/DailyChallenge/script.js
+++ Week8/Day5/DailyChallenge/script.js
@@ -25,3 +25,4 @@ function anagram(arr, arr2) {
 console.log(anagram('Astronomer', 'Moon starer'));
 console.log(anagram('School master', 'The classroom'));
 console.log(anagram('The Morse Code', 'Here come dots'));
+
--- Week8/Day5/MiniProject/js/script.js
+++ Week8/Day5/MiniProject/js/script.js
@@ -72,66 +72,6 @@ const robots = [
 ];
 
 
-// class Robots {
-//     constructor ({name, username, email, image}) {
-//         this.name = name;
-//         this.username = username;
-//         this.email = email;
-//         this.image = image;
-//     }
-
-//     displayRobots() {
-//         const row = document.querySelector('.row__content');
-// //card__outer
-//         const robotCard = document.createElement('div');
-//         robotCard.classList.add('card__item');
-// //card__inner
-//         const cardContent = document.createElement('div');
-//         cardContent.classList.add('card__content');
-// //card__image
-//         const robotImageDiv = document.createElement('div');
-//         robotImageDiv.classList.add('card__img');
-//         const robotImage = document.createElement('img');
-//         robotImage.src = this.image;
-// //card__title
-//         const robotTitle = document.createElement('h3');
-//         robotTitle.classList.add('card__title');
-//         const title = document.createTextNode(this.name);
-// //email
-//         const robotEmail = document.createElement('p');
-//         robotEmail.classList.add('card__email');
-//         const emailText = document.createTextNode(this.email);
-
-// //img
-//         robotImageDiv.appendChild(robotImage);
-//         cardContent.appendChild(robotImageDiv);
-// //title
-//         robotTitle.appendChild(title);
-//         cardContent.appendChild(robotTitle);
-// //email
-//         robotEmail.appendChild(emailText);
-//         cardContent.appendChild(robotEmail);
-
-// //display card in DOM
-//         robotCard.appendChild(cardContent);
-//         row.appendChild(robotCard);
-//     }
-
-
-
-// }
-
-// robots.forEach(element => {
-//     const robot = new Robots(element);
-//     robot.displayRobots();
-        
-        // const allRobotosObj = [];
-        // allRobotosObj.push(robot)
-// })
-
-
-
-
 const row = document.querySelector('.row__content');
 
 
@@ -179,22 +119,17 @@ function displayRobots(element) {
 
 function search() {
 
-    const input = document.querySelector('#header-Search');
-    input.addEventListener('input', () => {
+  const input = document.querySelector('#header-Search');
+  input.addEventListener('input', () => {
 
-        let results = robots.filter(element => element['name'].toLowerCase().includes(input.value));
+      let results = robots.filter(element => element['name'].toLowerCase().includes(input.value));
 //clear main
-        row.innerHTML = '';
+      row.innerHTML = '';
 
-        results.forEach(element => {
-            displayRobots(element);
-        })
-    })
+      results.forEach(element => {
+          displayRobots(element);
+      })
+  })
 }
 
 search();
-
-
-
-
-
