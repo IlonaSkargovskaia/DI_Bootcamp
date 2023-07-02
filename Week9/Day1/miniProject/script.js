359f11bbc0020506041800b95517518a8b97f5c6
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-02 21:01:34 +0300

    feat: 1-2 part of miniProject done

--- Week9/Day1/miniProject/script.js
+++ Week9/Day1/miniProject/script.js
@@ -2,17 +2,20 @@ const quotes = [
     {
         id : 0,
         author: 'Franklin D. Roosevelt',
-        quote: 'When you reach the end of your rope, tie a knot in it and hang on'
+        quote: 'When you reach the end of your rope, tie a knot in it and hang on',
+        likes: 0,
     },
     {
         id : 1,
         author: 'Anne Frank',
-        quote: 'Whoever is happy will make others happy too'
+        quote: 'Whoever is happy will make others happy too',
+        likes: 0,
     },
     {
         id : 2,
         author: 'Thomas A. Edison',
-        quote: 'Many of life\'s failures are people who did not realize how close they were to success when they gave up'
+        quote: 'Many of life\'s failures are people who did not realize how close they were to success when they gave up',
+        likes: 0,
     },
 ]
 
@@ -20,12 +23,12 @@ const quotes = [
 const section = document.querySelector('section');
 
 const btn = document.querySelector('#generate');
-btn.addEventListener(('click'), (event) => {
-    event.preventDefault();
-    
-    //random get 1 object
-    // const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    // console.log(quote);
+btn.addEventListener(('click'), generate);
+
+// Declare a variable to store the initially generated quote object
+let initialQuote;
+
+function generate (){
 
     //call function checker
     const quoteIndex = checkQuotes();
@@ -37,9 +40,15 @@ btn.addEventListener(('click'), (event) => {
 
     quoteElement.appendChild(quoteText);
     section.appendChild(quoteElement);
-})
+
+    // Store the initially generated quote object in the initialQuote variable
+    if (!initialQuote) {
+        initialQuote = quote;
+    }
+}
 
 
+//check
 let previousQuoteIndex = null;
 
 function checkQuotes(){
@@ -57,7 +66,7 @@ function checkQuotes(){
     return quoteIndex;
 }
 
-//2 part
+//2 part - add new
 
 const form = document.querySelector('#newQuotes');
 const inputeQuote = form.querySelector('#newQuote');
@@ -71,8 +80,43 @@ addBtn.addEventListener(('click'), (event) => {
     newObj['id'] = quotes.length; 
     newObj['author'] = inputeAuthor.value;
     newObj['quote'] = inputeQuote.value;
+    newObj['likes'] = 0;
 
     quotes.push(newObj);
     console.log(quotes);
 });
+
+
+//3 part - buttons
+
+const numCharacters = document.querySelector('#numCharacters');
+const numChWithoutSpaces = document.querySelector('#numChWithoutSpaces');
+const numWords = document.querySelector('#numWords');
+const likes = document.querySelector('#likes');
+
+numCharacters.addEventListener(('click'), () => {
+    
+    const textString = initialQuote['quote'];
+    console.log(`number of characters in quote: `, textString.length);
+    
+})
+
+numChWithoutSpaces.addEventListener(('click'), () => {
+    
+    const textString = initialQuote['quote'];
+    console.log(`number of characters in quote without spaces: `, textString.replace(/\s/g, '').length);
+})
+
+numWords.addEventListener('click', () => {
+    
+    const countWords = initialQuote['quote'];
+    console.log(`count of words: `, countWords.split(' ').length);
+})
+
+likes.addEventListener('click', () => {
+    
+    initialQuote.likes++;
+    console.log('Current likes: ', initialQuote.likes);
+    
+})
 
