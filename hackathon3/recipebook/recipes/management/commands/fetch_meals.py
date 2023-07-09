36a7a94b322b6e76746cdc24b3d82966fa886770
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-09 13:06:29 +0300

    feat: start exXP W10D1

--- hackathon3/recipebook/recipes/management/commands/fetch_meals.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from recipes.api import save_data_from_api
-
-class Command(BaseCommand):
-    help = 'Fetches recipes from the Spoonacular API and saves them to the database'
-
-    def handle(self, *args, **options):
-        save_data_from_api()
-        self.stdout.write(self.style.SUCCESS('Recipes saved successfully.'))
