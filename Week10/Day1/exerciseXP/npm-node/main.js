b959716253b22c6c95e7bf7aef1b2d3a0d77cc74
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-09 13:34:04 +0300

    feat: exercixeXP W10D1 done

---
+++ Week10/Day1/exerciseXP/npm-node/main.js
@@ -0,0 +1,26 @@
+// Create a new folder called - npm-node
+// Create a JS file called - main.js
+// Add the following code to the file main.js :
+
+// const a = 5;
+// const b = 10;
+
+
+// Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+
+const a = 5;
+const b = 10;
+
+console.log(`Hello you are ${a+b}`);
+
+
+// Create a package.json file - use npm init
+// Run the main.js file using npm start.
+// Tip: you need to change the package.json file in order to achieve this.
+// You should output something in the command prompt, for example: Hi! How are you ?
+
+
+console.log(`Hi! How are you ?`);
+
+
+//3 ex in another folder: npm-nodemon
