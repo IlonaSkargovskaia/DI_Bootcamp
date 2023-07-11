b68da900e45ca1caa34370e2af398cbf6940ee0d
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-12 00:11:25 +0300

    feat: exXP Day3W10 done

--- Week10/Day3/products-api/routes/products.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { _getAllProducts, _getProduct } from '../controllers/products.js';
-
-const prouter = express.Router();
-
-prouter.get('/', _getAllProducts)
-prouter.get('/:id', _getProduct)
-
-export default prouter;
