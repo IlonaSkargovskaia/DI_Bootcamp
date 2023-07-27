9c7b1f8a16071907f6410d9e35ca20b56828acb9
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-27 22:48:13 +0300

    feat: 60% todos done

--- Week11/Day5/quotes/src/components/QuotesComponent.js
+++ Week11/Day5/quotes/src/components/QuotesComponent.js
@@ -2,7 +2,8 @@ import quotes from '../quotesdata.js';
 import {useState, useEffect} from 'react';
 
 const QuotesComponent = () => {
-    const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+    const colorArray = [
+    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
     '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
     '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
     '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
@@ -20,11 +21,11 @@ const QuotesComponent = () => {
 
     const [quote, setQuote] = useState(quotes[0]);
     const [color, setColor] = useState(randomColor);
+    const [isDropIn, setIsDropIn] = useState(true);
 
 
     useEffect(() => {
         const randomObj = quotes[Math.floor(Math.random() * quotes.length)];
-        //console.log(randomObj);
         setQuote(randomObj);
 
         const color = randomColor;
@@ -35,20 +36,34 @@ const QuotesComponent = () => {
 
     const changeQuotes = () => {
         const randomObj = quotes[Math.floor(Math.random() * quotes.length)];
-        //console.log(randomObj);
         setQuote(randomObj);
 
         const color = randomColor;
         setColor(color);
         document.body.style.backgroundColor = color;
+
+        // Trigger the drop-in animation by toggling the 'isDropIn' state
+        setIsDropIn(true);
     }
 
+     // to handle the end of the drop-in animation
+    const handleDropInEnd = () => {
+        setIsDropIn(false);
+    };
+
+
     return (
-        <div className="quoteBlock">
-          
-            <h1 style={{color: color}}>"{quote.quote}"</h1>
-            <p><i>{(quote.author === '') ? '-- unknown --' : `-- ${quote.author} --`}</i></p>
-            <button onClick={changeQuotes} style={{backgroundColor: color}}>New Quote</button>
+        <div className="quoteBlock ">
+          <div className={`${isDropIn ? 'drop-in' : ''}`}
+                style={{ color: color }}
+                onAnimationEnd={handleDropInEnd}>
+
+                <h1 >"{quote.quote}"</h1>
+                <div style={{textAlign: 'right'}}>
+                    <p><i>{(quote.author === '') ? '-- unknown --' : `-- ${quote.author} --`}</i></p>
+                    <button onClick={changeQuotes} style={{backgroundColor: color}}>New Quote</button>
+                </div>
+          </div> 
         </div>
     )
 }
---
+++ Week11/Day5/todos/src/components/NewTask.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+
+const NewTask = (props) => {
+
+    const [task, setTask] = useState('');
+    const [list, setList] = useState([]);
+
+
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        if (task !== '') {
+            setList((prevList) => [...prevList, task]);
+            setTask('');   
+        }
+        
+    }
+
+    const handleInput = (e) => {
+        setTask(e.target.value);
+    }
+
+    return(
+        <>
+            <div className='todosBlock'>
+                
+                {
+                    (list.length === 0) ? 
+                        <div className='noTodos'>`:) You have no todo's left`</div>
+                    :
+                    list.map((item, index) => (
+                        <div key={index} className='newTask'>{item}</div>
+                    ))
+                    
+                }
+            </div>
+
+            
+            <form onSubmit={handleForm}>
+                <label>Add a new todo:</label>
+                <input type='text' onChange={handleInput} className='addTask'/>
+            </form>
+            
+        </>
+    )
+
+}
+
+export default NewTask;
