8dc15abf55fc046abbe51d7e7e4dd8822235ae0f
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-27 19:25:17 +0300

    feat: daily D4W11 done

--- Week11/Day4/daily-challenge/client/src/App.js
+++ Week11/Day4/daily-challenge/client/src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      header: null,
+      inputValue: '',
+      responseMessage: '',
     }
   }
 
@@ -22,14 +24,54 @@ class App extends Component {
     }
     getText();
   }
+
+  //post to my server
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:3005/api/world', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        //key can be whatever
+        body: JSON.stringify({ post: this.state.inputValue }),
+      });
+
+      if (res.ok) {
+        // Get the response message from the server
+        const responseMessage = await res.text();
+        //in browser console log
+        console.log('Data posted successfully');
+        // Reset the input after submit and set the response message from server
+        this.setState({ inputValue: "", responseMessage });
+      } else {
+        console.error("Response not 200");
+      }
+    } catch (error) {
+      console.error('Failed to post data');
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({inputValue: e.target.value})
+  }
   
   render() { 
+
+    const {inputValue, responseMessage} = this.state;
+
     return ( 
       <div className="App">
         <header>
           <h1>{this.state.header}</h1>
           <h2>Post to Server:</h2>
-          
+          <form onSubmit={this.handleSubmit}>
+              <input type="text" onChange={this.handleInput} value={inputValue}/>
+              <button>Submit</button>
+          </form>
+          <p>{responseMessage}</p>
         </header>
       </div>
     );
--- Week11/Day4/daily-challenge/server/server.js
+++ Week11/Day4/daily-challenge/server/server.js
@@ -11,7 +11,14 @@ app.get('/api/hello', (req, res) => {
     res.json('Hello from express');
 })
 
+app.post('/api/world', (req, res) => {
+
+    const {post} = req.body;
+    console.log(req.body);
+    res.json(`I received your POST request. This is what you sent me: ${post}`);
+})
+
 
 app.listen(3005, () => {
-    console.log(`listening 3001`);
+    console.log(`listening 3005`);
 })
