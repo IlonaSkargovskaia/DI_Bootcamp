4c83788097e8f022564d4223bf8a2acb439845e8
Ilona Skargovskaya <ilona.skars@gmail.com>
2023-07-28 07:24:22 +0300

    feat: memory game DONE

--- Week11/Day5/memory/src/App.js
+++ Week11/Day5/memory/src/App.js
@@ -1,23 +1,12 @@
 import Cards from "./components/Cards";
+import {useState} from 'react';
 import "./App.css";
 
 function App() {
+
     return (
         <div>
-            <header>
-                <div className="header__block">
-                    <div className="container">
-                        <h1>Superheroes Memory Game</h1>
-                        <div className='score__block'>
-                            <div className="score">Score: </div>
-                            <div className="score__top">Top score: </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="points__block">
-                        <h3>Get points by clicking on image, but don't click on any more than once!</h3>
-                </div>
-            </header>
+            
             <main>
                 <Cards />
             </main>
